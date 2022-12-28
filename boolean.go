0e0d0fdf3115e9bbb007af7adc503f97b85dab57
engkos97 <engkoskostaman759@gmail.com>
2022-12-28 19:11:42 +0700

    string

--- boolean.go
+++ boolean.go
@@ -5,5 +5,4 @@ import "fmt"
 func main(){
 	fmt.Println("Benar =", true)
 	fmt.Println("salah =", false)
-
 }
---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("engkos")
+	fmt.Println("engkos backend")
+	fmt.Println("engkos frontend")
+
+	fmt.Println(len("engkos"))
+	fmt.Println("engkos backend"[0])
+	fmt.Println("engkos frontend"[1])
+}
