09445f250e03ff62be7a421c593bad01159617ef
engkos97 <engkoskostaman759@gmail.com>
2022-12-28 20:00:40 +0700

    type-declarations

---
+++ type-declarations.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	type NoKTP string
+	type merried bool
+	var NoKTPEngkos NoKTP = "3232523523525"
+	var married merried = true 
+	fmt.Println(NoKTPEngkos)
+	fmt.Println(married)
+}
