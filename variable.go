575e45ffe61b19c77a0be47cc9b97dd1299d12fb
engkos97 <engkoskostaman759@gmail.com>
2022-12-28 19:28:03 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main (){
+	var name string 
+
+	name =" Engkos kostaman"
+	fmt.Println(name)
+
+	name =" engkos k"
+	fmt.Println(name)
+
+	var last ="engkos fullstack"
+	fmt.Println(last)
+
+	country := "argentina"
+	fmt.Println(country)
+
+	country = "indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName="engkos"
+		lastName="kostaman"
+	)
+    fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
