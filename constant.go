47c6d3a35994627ebf2373b9ba795ca951bc8396
engkos97 <engkoskostaman759@gmail.com>
2022-12-28 19:35:27 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main(){
+	const firstName string ="engkos"
+	const lastName ="kostaman"
+
+	const (
+		name="engkos"
+		age=25
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+}
