f90f76849d2d698dee5740d4add38b34b964eae2
engkos ragda <engkos.ragda@gmail.com>
2023-03-10 06:39:07 +0700

    switch

--- switch.go
+++ switch.go
@@ -16,4 +16,14 @@ func main() {
 
 	}
 
+	length := len(name)
+	switch {
+	case length > 10:
+		fmt.Println("Nama Terlalu Panjang")
+	case length > 5:
+		fmt.Println("Nama Lumayan Panjang")
+	default:
+		fmt.Println("NAMA")
+	}
+
 }
