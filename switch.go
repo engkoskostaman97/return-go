e3b3142c1bef005a5bafafec328ae95778f41513
engkos ragda <engkos.ragda@gmail.com>
2023-03-10 06:31:03 +0700

    switch

---
+++ switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Engkos"
+
+	switch name {
+	case "Engkos":
+		fmt.Println("Hello Engkos")
+	case "Joko":
+		fmt.Println("Hello Joko")
+	default:
+		fmt.Println("Hai")
+
+	}
+
+}
