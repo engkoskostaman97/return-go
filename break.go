6a5bc156a714664c330510231714bcbcb658b206
engkos ragda <engkos.ragda@gmail.com>
2023-03-11 13:10:30 +0700

    break and continue

---
+++ break.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// break
+//
+//	func main() {
+//		for i := 0; i < 10; i++ {
+//			if i == 5 {
+//				break
+//			}
+//			fmt.Println("Perulangan ke", i)
+//		}
+//	}
+//
+// continue
+func main() {
+	for i := 0; i < 10; i++ {
+		if i%2 == 1 {
+			continue
+		}
+		fmt.Println("Perulangan: ", i)
+	}
+}
